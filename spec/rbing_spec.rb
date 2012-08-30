a98e32078d5b317f63c693753c2a497a95c928b2
Travis Pew <travis@travispew.com>
2012-08-30 14:35:56 -0400

    Revert "Remove specs for unsupported sources"
    This reverts commit ac11cc0f1653331f887b605830a85c69b227602b.
    
    Conflicts:
    	spec/rbing_spec.rb

--- spec/rbing_spec.rb
+++ spec/rbing_spec.rb
@@ -8,6 +8,33 @@ describe "RBing" do
   
   it "should format a query properly with a hash" do
     @bing.send(:build_query, "ruby").should eq({:Query=>"'ruby'"})
+
+  it "should provide an ad search method" do
+    @bing.should respond_to(:ad)
+  end
+  
+  it "should provide an image search method" do
+    @bing.should respond_to(:image)
+  end
+  
+  it "should provide an instant_answer search method" do
+    @bing.should respond_to(:instant_answer)
+  end
+  
+  it "should provide a news search method" do
+    @bing.should respond_to(:news)
+  end
+  
+  it "should provide a phonebook search method" do
+    @bing.should respond_to(:phonebook)
+  end
+  
+  it "should provide a related_search method" do
+    @bing.should respond_to(:related_search)
+  end
+  
+  it "should provide an spell method" do
+    @bing.should respond_to(:spell)
   end
   
   it "should provide a web search method" do
